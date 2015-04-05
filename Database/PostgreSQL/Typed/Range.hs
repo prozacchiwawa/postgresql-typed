f750b28a26e0fcd06f3e1fce172792e58254d7ea
Dylan Simon <dylan@dylex.net>
2015-04-05 13:50:27 -0400

    More documentation for Range

--- Database/PostgreSQL/Typed/Range.hs
+++ Database/PostgreSQL/Typed/Range.hs
@@ -19,9 +19,13 @@ import qualified Text.Parsec as P
 
 import Database.PostgreSQL.Typed.Types
 
+-- |A end-point for a range, which may be nothing (infinity, NULL in PostgreSQL), open (inclusive), or closed (exclusive)
 data Bound a
-  = Unbounded
-  | Bounded Bool a
+  = Unbounded -- ^ Equivalent to @Bounded False ±Infinity@
+  | Bounded
+    { _boundClosed :: Bool -- ^ @True@ if the range includes this bound
+    , _bound :: a
+    }
   deriving (Eq)
 
 instance Functor Bound where
@@ -30,19 +34,31 @@ instance Functor Bound where
 
 newtype LowerBound a = Lower { boundLower :: Bound a } deriving (Eq, Functor)
 
+-- |Takes into account open vs. closed (but does not understand equivalent discrete bounds)
 instance Ord a => Ord (LowerBound a) where
   compare (Lower Unbounded) (Lower Unbounded) = EQ
   compare (Lower Unbounded) _ = LT
   compare _ (Lower Unbounded) = GT
   compare (Lower (Bounded ac a)) (Lower (Bounded bc b)) = compare a b <> compare bc ac
 
+-- |The constraint is only necessary for @maxBound@, unfortunately
+instance Bounded a => Bounded (LowerBound a) where
+  minBound = Lower Unbounded
+  maxBound = Lower (Bounded False maxBound)
+
 newtype UpperBound a = Upper { boundUpper :: Bound a } deriving (Eq, Functor)
 
+-- |Takes into account open vs. closed (but does not understand equivalent discrete bounds)
 instance Ord a => Ord (UpperBound a) where
   compare (Upper Unbounded) (Upper Unbounded) = EQ
   compare (Upper Unbounded) _ = GT
   compare _ (Upper Unbounded) = LT
   compare (Upper (Bounded ac a)) (Upper (Bounded bc b)) = compare a b <> compare ac bc
+
+-- |The constraint is only necessary for @minBound@, unfortunately
+instance Bounded a => Bounded (UpperBound a) where
+  minBound = Upper (Bounded False minBound)
+  maxBound = Upper Unbounded
 
 compareBounds :: Ord a => LowerBound a -> UpperBound a -> Bound Bool
 compareBounds (Lower (Bounded lc l)) (Upper (Bounded uc u)) =
@@ -75,27 +91,33 @@ bound :: Bound a -> Maybe a
 bound Unbounded = Nothing
 bound (Bounded _ b) = Just b
 
+-- |Unbounded endpoints are always open.
 boundClosed :: Bound a -> Bool
 boundClosed Unbounded = False
 boundClosed (Bounded c _) = c
 
+-- |Construct from parts: @makeBound (boundClosed b) (bound b) == b@
 makeBound :: Bool -> Maybe a -> Bound a
 makeBound c (Just a) = Bounded c a
 makeBound False Nothing = Unbounded
 makeBound True Nothing = error "makeBound: unbounded may not be closed"
 
+-- |Empty ranges treated as 'Unbounded'
 lowerBound :: Range a -> Bound a
 lowerBound Empty = Unbounded
 lowerBound (Range (Lower b) _) = b
 
+-- |Empty ranges treated as 'Unbounded'
 upperBound :: Range a -> Bound a
 upperBound Empty = Unbounded
 upperBound (Range _ (Upper b)) = b
 
+-- |Equivalent to @boundClosed . lowerBound@
 lowerClosed :: Range a -> Bool
 lowerClosed Empty = False
 lowerClosed (Range (Lower b) _) = boundClosed b
 
+-- |Equivalent to @boundClosed . upperBound@
 upperClosed :: Range a -> Bool
 upperClosed Empty = False
 upperClosed (Range _ (Upper b)) = boundClosed b
@@ -116,23 +138,29 @@ isFull :: Range a -> Bool
 isFull (Range (Lower Unbounded) (Upper Unbounded)) = True
 isFull _ = False
 
+-- |Create a point range @[x,x]@
 point :: Eq a => a -> Range a
 point a = Range (Lower (Bounded True a)) (Upper (Bounded True a))
 
+-- |Extract a point: @getPoint (point x) == Just x@
 getPoint :: Eq a => Range a -> Maybe a
 getPoint (Range (Lower (Bounded True l)) (Upper (Bounded True u))) = u <$ guard (u == l)
 getPoint _ = Nothing
 
+-- Construct a range from endpoints and normalize it.
 range :: Ord a => Bound a -> Bound a -> Range a
 range l u = normalize $ Range (Lower l) (Upper u)
 
+-- Construct a standard range (@[l,u)@ or 'point') from bounds (like 'bound') and normalize it.
 normal :: Ord a => Maybe a -> Maybe a -> Range a
 normal l u = range (mb True l) (mb (l == u) u) where
   mb = maybe Unbounded . Bounded
 
+-- Construct a bounded range like 'normal'.
 bounded :: Ord a => a -> a -> Range a
 bounded l u = normal (Just l) (Just u)
 
+-- Fold empty ranges to 'Empty'.
 normalize :: Ord a => Range a -> Range a
 normalize r
   | isEmpty r = Empty
@@ -141,7 +169,7 @@ normalize r
 -- |'normalize' for discrete (non-continuous) range types, using the 'Enum' instance
 normalize' :: (Ord a, Enum a) => Range a -> Range a
 normalize' Empty = Empty
-normalize' (Range (Lower l) (Upper u)) = range l' u'
+normalize' (Range (Lower l) (Upper u)) = normalize $ range l' u'
   where
   l' = case l of
     Bounded False b -> Bounded True (succ b)

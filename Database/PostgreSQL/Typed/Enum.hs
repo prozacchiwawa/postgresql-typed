prozacchiwawa/postgresql-typed

commit 01328ba653d2be293c038097a038f5fd0f329160

Add HDBC support, many protocol updates, tests

postgresql-typed.cabal
@@ -1,12 +1,12 @@
 Name:          postgresql-typed
-Version:       0.4.4
+Version:       0.5
 Cabal-Version: >= 1.8
 License:       BSD3
 License-File:  COPYING
-Copyright:     2010-2013 Chris Forno, 2014-2015 Dylan Simon
+Copyright:     2010-2013 Chris Forno, 2014-2016 Dylan Simon
 Author:        Dylan Simon
 Maintainer:    Dylan Simon <dylan-pgtyped@dylex.net>
-Stability:     beta
+Stability:     provisional
 Bug-Reports:   https://github.com/dylex/postgresql-typed/issues
 Homepage:      https://github.com/dylex/postgresql-typed
 Category:      Database
@@ -47,9 +47,12 @@ Flag aeson
   Description: Support decoding json via aeson.
   Default: True
 
+Flag HDBC
+  Description: Provide an HDBC driver
+
 Library
   Build-Depends:
-    base >= 4.7 && < 5,
+    base >= 4.8 && < 5,
     array,
     binary,
     containers,
@@ -96,13 +99,24 @@ Library
   if flag(aeson)
     Build-Depends: aeson >= 0.7
     CPP-options: -DUSE_AESON
+  if flag(HDBC)
+    Build-Depends: HDBC >= 2.2
+    Exposed-Modules:
+      Database.PostgreSQL.Typed.HDBC
 
 test-suite test
   build-depends: base, network, time, bytestring, postgresql-typed, QuickCheck
   type: exitcode-stdio-1.0
+  hs-source-dirs: test
   main-is: Main.hs
   Other-Modules: Connect
-  buildable: True
-  hs-source-dirs: test
   Extensions: TemplateHaskell, QuasiQuotes
   GHC-Options: -Wall
+
+test-suite hdbc
+  build-depends: base, network, time, containers, convertible, postgresql-typed, HDBC, HUnit
+  type: exitcode-stdio-1.0
+  hs-source-dirs: test/hdbc, test
+  main-is: runtests.hs
+  if !flag(HDBC)
+    buildable: False

Database/PostgreSQL/Typed/Dynamic.hs
@@ -95,6 +95,7 @@ instance PGRep "text" T.Text
 #endif
 instance PGRep "date" Time.Day
 instance PGRep "time without time zone" Time.TimeOfDay
+instance PGRep "time with time zone" (Time.TimeOfDay, Time.TimeZone)
 instance PGRep "timestamp without time zone" Time.LocalTime
 instance PGRep "timestamp with time zone" Time.UTCTime
 instance PGRep "interval" Time.DiffTime

Database/PostgreSQL/Typed/HDBC.hs
@@ -1,0 +1,341 @@
+-- |
+-- Module: Database.PostgreSQL.Typed.HDBC
+-- Copyright: 2016 Dylan Simon
+-- 
+-- Use postgresql-typed as a backend for HDBC.
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+module Database.PostgreSQL.Typed.HDBC
+  ( Connection, pgConnection
+  , connect
+  , reloadTypes
+  ) where
+
+import Control.Arrow ((&&&))
+import Control.Concurrent.MVar (MVar, newMVar, readMVar, withMVar)
+import Control.Exception (handle, throwIO)
+import Control.Monad (void, guard)
+import qualified Data.ByteString.Char8 as BSC
+import qualified Data.ByteString.Lazy.Char8 as BSLC
+import Data.IORef (newIORef, readIORef, writeIORef, modifyIORef')
+import Data.Int (Int16)
+import qualified Data.IntMap.Lazy as IntMap
+import Data.List (uncons)
+import qualified Data.Map.Lazy as Map
+import Data.Maybe (fromMaybe, isNothing)
+import Data.Time.Clock (DiffTime)
+import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
+import Data.Time.LocalTime (zonedTimeToUTC)
+import Data.Word (Word32)
+import qualified Database.HDBC.Types as HDBC
+import qualified Database.HDBC.ColTypes as HDBC
+import System.Mem.Weak (addFinalizer)
+import Text.Read (readMaybe)
+
+import Database.PostgreSQL.Typed.Protocol
+import Database.PostgreSQL.Typed.Types
+import Database.PostgreSQL.Typed.Dynamic
+import Database.PostgreSQL.Typed.TH
+import Database.PostgreSQL.Typed.SQLToken
+import Paths_postgresql_typed (version)
+
+-- |A wrapped 'PGConnection'.
+-- This differs from a bare 'PGConnection' in two ways:
+--
+--   1) It always has exactly one active transaction (with 'pgBegin')
+--   2) It automatically disconnects on GC
+--
+data Connection = Connection
+  { pgConnection :: MVar PGConnection -- ^Access the underlying 'PGConnection' directly. You must be careful to ensure that the first invariant is preserved: you should not call 'pgBegin', 'pgCommit', or 'pgRollback' on it. All other operations should be safe.
+  , pgServerVer :: String
+  , pgTypes :: IntMap.IntMap SqlType
+  }
+
+sqlError :: IO a -> IO a
+sqlError = handle $ \(PGError m) -> 
+  let f c = BSC.unpack $ Map.findWithDefault BSC.empty c m
+      fC = f 'C'
+      fD = f 'D' in
+  throwIO HDBC.SqlError 
+    { HDBC.seState = fC
+    , HDBC.seNativeError = if null fC then -1 else fromMaybe 0 $ readMaybe (f 'P')
+    , HDBC.seErrorMsg = f 'S' ++ ": " ++ f 'M' ++ if null fD then fD else '\n':fD
+    }
+
+withPG :: Connection -> (PGConnection -> IO a) -> IO a
+withPG c = sqlError . withMVar (pgConnection c)
+
+connect_ :: PGDatabase -> IO PGConnection
+connect_ d = sqlError $ do
+  pg <- pgConnect d
+  addFinalizer pg (pgDisconnectOnce pg)
+  pgBegin pg
+  return pg
+
+-- |Connect to a database for HDBC use (equivalent to 'pgConnect' and 'pgBegin').
+connect :: PGDatabase -> IO Connection
+connect d = do
+  pg <- connect_ d
+  pgv <- newMVar pg
+  reloadTypes Connection
+    { pgConnection = pgv
+    , pgServerVer = maybe "" BSC.unpack $ pgServerVersion pg
+    , pgTypes = mempty
+    }
+
+-- |Reload the table of all types from the database.
+-- This may be needed if you make structural changes to the database.
+reloadTypes :: Connection -> IO Connection
+reloadTypes c = withPG c $ \pg -> do
+  t <- pgLoadTypes pg
+  return c{ pgTypes = IntMap.map (sqlType $ pgTypeEnv pg) t }
+
+sqls :: String -> BSLC.ByteString
+sqls = BSLC.pack
+
+placeholders :: Int -> [SQLToken] -> [SQLToken]
+placeholders n (SQLQMark False : l) = SQLParam n : placeholders (succ n) l
+placeholders n (SQLQMark True : l) = SQLQMark False : placeholders n l
+placeholders n (t : l) = t : placeholders n l
+placeholders _ [] = []
+
+data ColDesc = ColDesc
+  { colDescName :: String
+  , colDesc :: HDBC.SqlColDesc
+  , colDescDecode :: PGValue -> HDBC.SqlValue
+  }
+
+data Cursor = Cursor
+  { cursorDesc :: [ColDesc]
+  , cursorRow :: [PGValues]
+  , cursorActive :: Bool
+  , _cursorStatement :: HDBC.Statement -- keep a handle to prevent GC
+  }
+
+noCursor :: HDBC.Statement -> Cursor
+noCursor = Cursor [] [] False
+
+-- |Number of rows to retrieve (and cache) with each call to fetchRow.
+-- Ideally this should be configurable, but it's not clear how.
+fetchSize :: Word32
+fetchSize = 1
+
+getType :: Connection -> PGConnection -> Maybe Bool -> PGColDescription -> ColDesc
+getType c pg nul PGColDescription{..} = ColDesc
+  { colDescName = BSC.unpack colName
+  , colDesc = HDBC.SqlColDesc
+    { HDBC.colType = sqlTypeId t
+    , HDBC.colSize = fromIntegral colModifier <$ guard (colModifier >= 0)
+    , HDBC.colOctetLength = fromIntegral colSize <$ guard (colSize >= 0)
+    , HDBC.colDecDigits = Nothing
+    , HDBC.colNullable = nul
+    }
+  , colDescDecode = sqlTypeDecode t
+  } where t = IntMap.findWithDefault (sqlType (pgTypeEnv pg) $ show colType) (fromIntegral colType) (pgTypes c)
+
+instance HDBC.IConnection Connection where
+  disconnect c = withPG c
+    pgDisconnectOnce
+  commit c = withPG c $ \pg -> do
+    pgCommit pg
+    pgBegin pg
+  rollback c = withPG c $ \pg -> do
+    pgRollback pg
+    pgBegin pg
+  runRaw c q = withPG c $ \pg ->
+    pgSimpleQueries_ pg $ sqls q
+  run c q v = withPG c $ \pg -> do
+    let q' = sqls $ show $ placeholders 1 $ sqlTokens q
+        v' = map encode v
+    fromMaybe 0 <$> pgRun pg q' [] v'
+  prepare c q = do
+    let q' = sqls $ show $ placeholders 1 $ sqlTokens q
+    n <- withPG c $ \pg -> pgPrepare pg q' []
+    cr <- newIORef $ error "Cursor"
+    let
+      execute v = withPG c $ \pg -> do
+        d <- pgBind pg n (map encode v)
+        (r, e) <- pgFetch pg n fetchSize
+        modifyIORef' cr $ \p -> p
+          { cursorDesc = map (getType c pg Nothing) d
+          , cursorRow = r
+          , cursorActive = isNothing e
+          }
+        return $ fromMaybe 0 e
+      stmt = HDBC.Statement
+        { HDBC.execute = execute
+        , HDBC.executeRaw = void $ execute []
+        , HDBC.executeMany = mapM_ execute
+        , HDBC.finish = withPG c $ \pg -> do
+          writeIORef cr $ noCursor stmt
+          pgClose pg n
+        , HDBC.fetchRow = withPG c $ \pg -> do
+          p <- readIORef cr
+          fmap (zipWith colDescDecode (cursorDesc p)) <$> case cursorRow p of
+            [] | True || cursorActive p -> do
+                (rl, e) <- pgFetch pg n fetchSize
+                let rl' = uncons rl
+                writeIORef cr p
+                  { cursorRow = maybe [] snd rl'
+                  , cursorActive = isNothing e
+                  }
+                return $ fst <$> rl'
+               | otherwise ->
+                return Nothing
+            (r:l) -> do
+              writeIORef cr p{ cursorRow = l }
+              return $ Just r
+        , HDBC.getColumnNames =
+          map colDescName . cursorDesc <$> readIORef cr
+        , HDBC.originalQuery = q
+        , HDBC.describeResult =
+          map (colDescName &&& colDesc) . cursorDesc <$> readIORef cr
+        }
+    writeIORef cr $ noCursor stmt
+    addFinalizer stmt $ withPG c $ \pg -> pgClose pg n
+    return stmt
+  clone c = do
+    c' <- connect_ . pgConnectionDatabase =<< readMVar (pgConnection c)
+    cv <- newMVar c'
+    return c{ pgConnection = cv }
+  hdbcDriverName _ = "postgresql-typed"
+  hdbcClientVer _ = show version
+  proxiedClientName = HDBC.hdbcDriverName
+  proxiedClientVer = HDBC.hdbcClientVer
+  dbServerVer = pgServerVer
+  dbTransactionSupport _ = True
+  getTables c = withPG c $ \pg ->
+    map (pgDecodeRep . head) . snd <$> pgSimpleQuery pg (BSLC.fromChunks
+      [ "SELECT relname "
+      ,   "FROM pg_class "
+      ,   "JOIN pg_namespace "
+      ,     "ON relnamespace = pg_namespace.oid "
+      ,  "WHERE nspname = ANY (current_schemas(false)) "
+      ,    "AND relkind IN ('r','v','m','f')"
+      ])
+  describeTable c t = withPG c $ \pg -> do
+    let makecol ~[attname, attrelid, attnum, atttypid, attlen, atttypmod, attnotnull] =
+          colDescName &&& colDesc $ getType c pg (Just $ not $ pgDecodeRep attnotnull) PGColDescription
+            { colName = pgDecodeRep attname
+            , colTable = pgDecodeRep attrelid
+            , colNumber = pgDecodeRep attnum
+            , colType = pgDecodeRep atttypid
+            , colSize = pgDecodeRep attlen
+            , colModifier = pgDecodeRep atttypmod
+            , colBinary = False
+            }
+    map makecol . snd <$> pgSimpleQuery pg (BSLC.fromChunks
+      [ "SELECT attname, attrelid, attnum, atttypid, attlen, atttypmod, attnotnull "
+      ,   "FROM pg_attribute "
+      ,   "JOIN pg_class "
+      ,     "ON attrelid = pg_class.oid "
+      ,   "JOIN pg_namespace "
+      ,     "ON relnamespace = pg_namespace.oid "
+      ,  "WHERE nspname = ANY (current_schemas(false)) "
+      ,    "AND relkind IN ('r','v','m','f') "
+      ,    "AND relname = ", pgLiteralRep t
+      ,   " AND attnum > 0 AND NOT attisdropped "
+      ,    "ORDER BY attnum"
+      ])
+
+encodeRep :: (PGParameter t a, PGRep t a) => a -> PGValue
+encodeRep x = PGTextValue $ pgEncode (pgTypeOf x) x
+
+encode :: HDBC.SqlValue -> PGValue
+encode (HDBC.SqlString x)                 = encodeRep x
+encode (HDBC.SqlByteString x)             = encodeRep x
+encode (HDBC.SqlWord32 x)                 = encodeRep x
+encode (HDBC.SqlWord64 x)                 = encodeRep (fromIntegral x :: Rational)
+encode (HDBC.SqlInt32 x)                  = encodeRep x
+encode (HDBC.SqlInt64 x)                  = encodeRep x
+encode (HDBC.SqlInteger x)                = encodeRep (fromInteger x :: Rational)
+encode (HDBC.SqlChar x)                   = encodeRep x
+encode (HDBC.SqlBool x)                   = encodeRep x
+encode (HDBC.SqlDouble x)                 = encodeRep x
+encode (HDBC.SqlRational x)               = encodeRep x
+encode (HDBC.SqlLocalDate x)              = encodeRep x
+encode (HDBC.SqlLocalTimeOfDay x)         = encodeRep x
+encode (HDBC.SqlZonedLocalTimeOfDay t z)  = encodeRep (t, z)
+encode (HDBC.SqlLocalTime x)              = encodeRep x
+encode (HDBC.SqlZonedTime x)              = encodeRep (zonedTimeToUTC x)
+encode (HDBC.SqlUTCTime x)                = encodeRep x
+encode (HDBC.SqlDiffTime x)               = encodeRep (realToFrac x :: DiffTime)
+encode (HDBC.SqlPOSIXTime x)              = encodeRep (realToFrac x :: Rational) -- (posixSecondsToUTCTime x)
+encode (HDBC.SqlEpochTime x)              = encodeRep (posixSecondsToUTCTime (fromInteger x))
+encode (HDBC.SqlTimeDiff x)               = encodeRep (fromIntegral x :: DiffTime)
+encode HDBC.SqlNull = PGNullValue
+
+data SqlType = SqlType
+  { sqlTypeId :: HDBC.SqlTypeId
+  , sqlTypeDecode :: PGValue -> HDBC.SqlValue
+  }
+
+sqlType :: PGTypeEnv -> String -> SqlType
+sqlType e t = SqlType
+  { sqlTypeId = typeId t
+  , sqlTypeDecode = decode t e
+  }
+
+typeId :: String -> HDBC.SqlTypeId
+typeId "boolean"                      = HDBC.SqlBitT
+typeId "bytea"                        = HDBC.SqlVarBinaryT
+typeId "\"char\""                     = HDBC.SqlCharT
+typeId "name"                         = HDBC.SqlVarCharT
+typeId "bigint"                       = HDBC.SqlBigIntT
+typeId "smallint"                     = HDBC.SqlSmallIntT
+typeId "integer"                      = HDBC.SqlIntegerT
+typeId "text"                         = HDBC.SqlLongVarCharT
+typeId "oid"                          = HDBC.SqlIntegerT
+typeId "real"                         = HDBC.SqlFloatT
+typeId "double precision"             = HDBC.SqlDoubleT
+typeId "abstime"                      = HDBC.SqlUTCDateTimeT
+typeId "reltime"                      = HDBC.SqlIntervalT HDBC.SqlIntervalSecondT
+typeId "tinterval"                    = HDBC.SqlIntervalT HDBC.SqlIntervalDayToSecondT
+typeId "bpchar"                       = HDBC.SqlVarCharT
+typeId "character varying"            = HDBC.SqlVarCharT
+typeId "date"                         = HDBC.SqlDateT
+typeId "time without time zone"       = HDBC.SqlTimeT
+typeId "timestamp without time zone"  = HDBC.SqlTimestampT
+typeId "timestamp with time zone"     = HDBC.SqlTimestampWithZoneT -- XXX really SQLUTCDateTimeT
+typeId "interval"                     = HDBC.SqlIntervalT HDBC.SqlIntervalDayToSecondT
+typeId "time with time zone"          = HDBC.SqlTimeWithZoneT
+typeId "numeric"                      = HDBC.SqlDecimalT
+typeId "uuid"                         = HDBC.SqlGUIDT
+typeId t = HDBC.SqlUnknownT t
+
+decodeRep :: PGColumn t a => PGTypeName t -> PGTypeEnv -> (a -> HDBC.SqlValue) -> PGValue -> HDBC.SqlValue
+decodeRep t e f (PGBinaryValue v) = f $ pgDecodeBinary e t v
+decodeRep t _ f (PGTextValue v) = f $ pgDecode t v
+decodeRep _ _ _ PGNullValue = HDBC.SqlNull
+
+#define DECODE(T) \
+  decode T e = decodeRep (PGTypeProxy :: PGTypeName T) e
+
+decode :: String -> PGTypeEnv -> PGValue -> HDBC.SqlValue
+DECODE("boolean")                     HDBC.SqlBool
+DECODE("\"char\"")                    HDBC.SqlChar
+DECODE("name")                        HDBC.SqlString
+DECODE("bigint")                      HDBC.SqlInt64
+DECODE("smallint")                    (HDBC.SqlInt32 . fromIntegral :: Int16 -> HDBC.SqlValue)
+DECODE("integer")                     HDBC.SqlInt32
+DECODE("text")                        HDBC.SqlString
+DECODE("oid")                         HDBC.SqlWord32
+DECODE("real")                        HDBC.SqlDouble
+DECODE("double precision")            HDBC.SqlDouble
+DECODE("bpchar")                      HDBC.SqlString
+DECODE("character varying")           HDBC.SqlString
+DECODE("date")                        HDBC.SqlLocalDate
+DECODE("time without time zone")      HDBC.SqlLocalTimeOfDay
+DECODE("time with time zone")         (uncurry HDBC.SqlZonedLocalTimeOfDay)
+DECODE("timestamp without time zone") HDBC.SqlLocalTime
+DECODE("timestamp with time zone")    HDBC.SqlUTCTime
+DECODE("interval")                    (HDBC.SqlDiffTime . realToFrac :: DiffTime -> HDBC.SqlValue)
+DECODE("numeric")                     HDBC.SqlRational
+decode _ _ = decodeRaw where
+  decodeRaw (PGBinaryValue v) = HDBC.SqlByteString v
+  decodeRaw (PGTextValue v)   = HDBC.SqlByteString v
+  decodeRaw PGNullValue       = HDBC.SqlNull

Database/PostgreSQL/Typed/SQLToken.hs
@@ -13,6 +13,7 @@ module Database.PostgreSQL.Typed.SQLToken
 import Control.Arrow (first)
 import Data.Char (isDigit, isAsciiUpper, isAsciiLower)
 import Data.List (stripPrefix)
+import Data.String (IsString(..))
 
 -- |A parsed SQL token.
 data SQLToken
@@ -20,6 +21,7 @@ data SQLToken
   | SQLParam Int -- ^A \"$N\" parameter placeholder (this is the only non-string-preserving token: \"$012\" becomes \"$12\")
   | SQLExpr String -- ^A \"${expr}\" expression placeholder
   | SQLQMark Bool -- ^A possibly-escaped question-mark: False for \"?\" or True for \"\\?\"
+  deriving (Eq)
 
 -- |Produces the original SQL string
 instance Show SQLToken where
@@ -29,6 +31,9 @@ instance Show SQLToken where
   showsPrec _ (SQLQMark False) = showChar '?'
   showsPrec _ (SQLQMark True) = showString "\\?"
   showList = flip $ foldr shows
+
+instance IsString SQLToken where
+  fromString = SQLToken
 
 type PH = String -> [SQLToken]
 

Database/PostgreSQL/Typed/TH.hs
@@ -16,6 +16,9 @@ module Database.PostgreSQL.Typed.TH
   , tpgTypeEncoder
   , tpgTypeDecoder
   , tpgTypeBinary
+  -- * HDBC support
+  , PGTypes
+  , pgLoadTypes
   ) where
 
 #if !MIN_VERSION_base(4,8,0)
@@ -74,13 +77,21 @@ tpgState = unsafePerformIO $ do
 
 data TPGState = TPGState
   { tpgConnection :: PGConnection
-  , tpgTypes :: IntMap.IntMap TPGType -- keyed on fromIntegral OID
+  , tpgTypes :: PGTypes
   }
+
+-- |Map keyed on fromIntegral OID.
+type PGTypes = IntMap.IntMap TPGType
+
+-- |Load a map of types from the database.
+pgLoadTypes :: PGConnection -> IO PGTypes
+pgLoadTypes c =
+  IntMap.fromAscList . map (\[to, tn] -> (fromIntegral (pgDecodeRep to :: OID), pgDecodeRep tn)) .
+    snd <$> pgSimpleQuery c (BSLC.pack "SELECT typ.oid, format_type(CASE WHEN typtype = 'd' THEN typbasetype ELSE typ.oid END, -1) FROM pg_catalog.pg_type typ JOIN pg_catalog.pg_namespace nsp ON typnamespace = nsp.oid WHERE nspname <> 'pg_toast' AND nspname <> 'information_schema' ORDER BY typ.oid")
 
 tpgLoadTypes :: TPGState -> IO TPGState
 tpgLoadTypes tpg = do
-  t <- IntMap.fromAscList . map (\[to, tn] -> (fromIntegral (pgDecodeRep to :: OID), pgDecodeRep tn)) .
-    snd <$> pgSimpleQuery (tpgConnection tpg) (BSLC.pack "SELECT typ.oid, format_type(CASE WHEN typtype = 'd' THEN typbasetype ELSE typ.oid END, -1) FROM pg_catalog.pg_type typ JOIN pg_catalog.pg_namespace nsp ON typnamespace = nsp.oid WHERE nspname <> 'pg_toast' AND nspname <> 'information_schema' ORDER BY typ.oid")
+  t <- pgLoadTypes (tpgConnection tpg)
   return tpg{ tpgTypes = t }
 
 tpgInit :: PGConnection -> IO TPGState
@@ -123,7 +134,7 @@ reloadTPGTypes = TH.runIO $ [] <$ modifyMVar_ tpgState (\(d, c) -> (,) d <$> Tv.
 -- Error if not found.
 tpgType :: TPGState -> OID -> TPGType
 tpgType TPGState{ tpgTypes = types } t =
-  IntMap.findWithDefault (error $ "Unknown PostgreSQL type: " ++ show t) (fromIntegral t) types
+  IntMap.findWithDefault (error $ "Unknown PostgreSQL type: " ++ show t ++ "\nYour postgresql-typed application may need to be rebuilt.") (fromIntegral t) types
 
 -- |Lookup a type OID by type name.
 -- This is less common and thus less efficient than going the other way.

Database/PostgreSQL/Typed/Types.hs
@@ -40,6 +40,7 @@ module Database.PostgreSQL.Typed.Types
 #if !MIN_VERSION_base(4,8,0)
 import Control.Applicative ((<$>), (<$), (<*), (*>))
 #endif
+import Control.Arrow ((&&&))
 #ifdef USE_AESON
 import qualified Data.Aeson as JSON
 #endif
@@ -163,11 +164,6 @@ instance PGColumn t a => PGColumn t (Maybe a) where
   pgDecodeValue _ _ PGNullValue = Nothing
   pgDecodeValue e t v = Just $ pgDecodeValue e t v
 
-instance PGType t => PGColumn t PGValue where
-  pgDecode _ = PGTextValue
-  pgDecodeBinary _ _ = PGBinaryValue
-  pgDecodeValue _ _ = id
-
 -- |Final parameter encoding function used when a (nullable) parameter is passed to a prepared query.
 pgEncodeParameter :: PGParameter t a => PGTypeEnv -> PGTypeName t -> a -> PGValue
 pgEncodeParameter = pgEncodeValue
@@ -237,6 +233,17 @@ binDec d t = either (\e -> error $ "pgDecodeBinary " ++ pgTypeName t ++ ": " ++ 
 #define BIN_DEC(F)
 #endif
 
+instance PGType "any"
+instance PGType t => PGColumn t PGValue where
+  pgDecode _ = PGTextValue
+  pgDecodeBinary _ _ = PGBinaryValue
+  pgDecodeValue _ _ = id
+instance PGParameter "any" PGValue where
+  pgEncode _ (PGTextValue v) = v
+  pgEncode _ PGNullValue = error "pgEncode any: NULL"
+  pgEncode _ (PGBinaryValue _) = error "pgEncode any: binary"
+  pgEncodeValue _ _ = id
+
 instance PGType "void"
 instance PGColumn "void" () where
   pgDecode _ _ = ()
@@ -302,12 +309,19 @@ instance PGParameter "real" Float where
 instance PGColumn "real" Float where
   pgDecode _ = read . BSC.unpack
   BIN_DEC(binDec BinD.float4)
+instance PGColumn "real" Double where
+  pgDecode _ = read . BSC.unpack
+  BIN_DEC((realToFrac .) . binDec BinD.float4)
 
 instance PGType "double precision" where BIN_COL
 instance PGParameter "double precision" Double where
   pgEncode _ = BSC.pack . show
   pgLiteral = pgEncode
   BIN_ENC(BinE.float8)
+instance PGParameter "double precision" Float where
+  pgEncode _ = BSC.pack . show
+  pgLiteral = pgEncode
+  BIN_ENC(BinE.float8 . realToFrac)
 instance PGColumn "double precision" Double where
   pgDecode _ = read . BSC.unpack
   BIN_DEC(binDec BinD.float8)
@@ -468,6 +482,16 @@ binDecDatetime fi _ PGTypeEnv{ pgIntegerDatetimes = Just True } = binDec fi
 binDecDatetime _ _ PGTypeEnv{ pgIntegerDatetimes = Nothing } = error "pgDecodeBinary: unknown integer_datetimes value"
 #endif
 
+-- PostgreSQL uses "[+-]HH[:MM]" timezone offsets, while "%z" uses "+HHMM" by default.
+-- readTime can successfully parse both formats, but PostgreSQL needs the colon.
+fixTZ :: String -> String
+fixTZ "" = ""
+fixTZ ['+',h1,h2] | isDigit h1 && isDigit h2 = ['+',h1,h2,':','0','0']
+fixTZ ['-',h1,h2] | isDigit h1 && isDigit h2 = ['-',h1,h2,':','0','0']
+fixTZ ['+',h1,h2,m1,m2] | isDigit h1 && isDigit h2 && isDigit m1 && isDigit m2 = ['+',h1,h2,':',m1,m2]
+fixTZ ['-',h1,h2,m1,m2] | isDigit h1 && isDigit h2 && isDigit m1 && isDigit m2 = ['-',h1,h2,':',m1,m2]
+fixTZ (c:s) = c:fixTZ s
+
 instance PGType "time without time zone" where
   pgBinaryColumn = binColDatetime
 instance PGParameter "time without time zone" Time.TimeOfDay where
@@ -480,6 +504,20 @@ instance PGColumn "time without time zone" Time.TimeOfDay where
   pgDecode _ = readTime "%T%Q" . BSC.unpack
 #ifdef USE_BINARY
   pgDecodeBinary = binDecDatetime BinD.time_int BinD.time_float
+#endif
+
+instance PGType "time with time zone" where
+  pgBinaryColumn = binColDatetime
+instance PGParameter "time with time zone" (Time.TimeOfDay, Time.TimeZone) where
+  pgEncode _ (t, z) = BSC.pack $ Time.formatTime defaultTimeLocale "%T%Q" t ++ fixTZ (Time.formatTime defaultTimeLocale "%z" z)
+  pgLiteral t = pgQuoteUnsafe . pgEncode t
+#ifdef USE_BINARY
+  pgEncodeValue = binEncDatetime BinE.timetz_int BinE.timetz_float
+#endif
+instance PGColumn "time with time zone" (Time.TimeOfDay, Time.TimeZone) where
+  pgDecode _ = (Time.localTimeOfDay . Time.zonedTimeToLocalTime &&& Time.zonedTimeZone) . readTime "%T%Q%z" . fixTZ . BSC.unpack
+#ifdef USE_BINARY
+  pgDecodeBinary = binDecDatetime BinD.timetz_int BinD.timetz_float
 #endif
 
 instance PGType "timestamp without time zone" where
@@ -495,16 +533,6 @@ instance PGColumn "timestamp without time zone" Time.LocalTime where
 #ifdef USE_BINARY
   pgDecodeBinary = binDecDatetime BinD.timestamp_int BinD.timestamp_float
 #endif
-
--- PostgreSQL uses "[+-]HH[:MM]" timezone offsets, while "%z" uses "+HHMM" by default.
--- readTime can successfully parse both formats, but PostgreSQL needs the colon.
-fixTZ :: String -> String
-fixTZ "" = ""
-fixTZ ['+',h1,h2] | isDigit h1 && isDigit h2 = ['+',h1,h2,':','0','0']
-fixTZ ['-',h1,h2] | isDigit h1 && isDigit h2 = ['-',h1,h2,':','0','0']
-fixTZ ['+',h1,h2,m1,m2] | isDigit h1 && isDigit h2 && isDigit m1 && isDigit m2 = ['+',h1,h2,':',m1,m2]
-fixTZ ['-',h1,h2,m1,m2] | isDigit h1 && isDigit h2 && isDigit m1 && isDigit m2 = ['-',h1,h2,':',m1,m2]
-fixTZ (c:s) = c:fixTZ s
 
 instance PGType "timestamp with time zone" where
   pgBinaryColumn = binColDatetime

commit b56bb83a117f7d0ab94a8aaa306eca13479140ea

Ignore duplicate ReadyForQuery after Sync
Strangely enough, this is the second time I've fixed this bug, the first
being dd510bb1c57ba8b5a887d73690c373c2db307d0f, which I broke again in
01328ba653d2be293c038097a038f5fd0f329160, so now I'm suspicious.  This
appears to happen as a race-condition when the ready hasn't arrived and
we send a sync, though really we should know we've sent a sync, but oh
well, should be no harm in ignoring it.

COPYING
@@ -1,4 +1,4 @@
-Copyright (c) 2014-2016, Dylan Simon
+Copyright (c) 2014-2017, Dylan Simon
 Portions Copyright (c) 2010, 2011, Chris Forno
 All rights reserved.
 

Database/PostgreSQL/Typed/Enum.hs
@@ -9,7 +9,7 @@
 -- 
 -- Support for PostgreSQL enums.
 
-module Database.PostgreSQL.Typed.Enum 
+module Database.PostgreSQL.Typed.Enum
   ( PGEnum(..)
   , dataPGEnum
   ) where
@@ -55,14 +55,14 @@ class (Eq a, Ord a, Enum a, Bounded a, PGRep a) => PGEnum a where
 -- > instance PGEnum Foo where pgEnumValues = [(Foo_abc, "abc"), (Foo_DEF, "DEF")]
 --
 -- Requires language extensions: TemplateHaskell, FlexibleInstances, MultiParamTypeClasses, DeriveDataTypeable, DataKinds, TypeFamilies
-dataPGEnum :: String -- ^ Haskell type to create 
+dataPGEnum :: String -- ^ Haskell type to create
   -> PGName -- ^ PostgreSQL enum type name
   -> (String -> String) -- ^ How to generate constructor names from enum values, e.g. @(\"Type_\"++)@ (input is 'pgNameString')
   -> TH.DecsQ
 dataPGEnum typs pgenum valnf = do
   (pgid, vals) <- TH.runIO $ withTPGTypeConnection $ \tpg -> do
     vals <- map (\([eo, v]) -> (pgDecodeRep eo, pgDecodeRep v)) . snd
-      <$> pgSimpleQuery (pgConnection tpg) (BSL.fromChunks 
+      <$> pgSimpleQuery (pgConnection tpg) (BSL.fromChunks
         [ "SELECT enumtypid, enumlabel"
         ,  " FROM pg_catalog.pg_enum"
         , " WHERE enumtypid = ", pgLiteralRep pgenum, "::regtype"

Database/PostgreSQL/Typed/Protocol.hs
@@ -411,22 +411,26 @@ pgRecv :: Bool -> PGConnection -> IO (Maybe PGBackendMessage)
 pgRecv block c@PGConnection{ connHandle = h, connInput = dr, connState = sr } =
   go =<< readIORef dr where
   next = writeIORef dr
-  state s d = writeIORef sr s >> next d
   new = G.pushChunk getMessage
   go (G.Done b _ m) = do
     when (connDebug c) $ putStrLn $ "< " ++ show m
-    got (new b) m =<< readIORef sr
+    got (new b) m
   go (G.Fail _ _ r) = next (new BS.empty) >> fail r -- not clear how can recover
   go d@(G.Partial r) = do
     b <- (if block then BS.hGetSome else BS.hGetNonBlocking) h smallChunkSize
     if BS.null b
       then Nothing <$ next d
       else go $ r (Just b)
-  got :: G.Decoder PGBackendMessage -> PGBackendMessage -> PGState -> IO (Maybe PGBackendMessage)
-  got d (NoticeResponse m) _ = connLogMessage c m >> go d
-  got d m@(ReadyForQuery s) _ = Just m <$ state s d
-  got d m@(ErrorResponse _) _ = Just m <$ state StateUnsync d
-  got d m _ = Just m <$ next d
+  got :: G.Decoder PGBackendMessage -> PGBackendMessage -> IO (Maybe PGBackendMessage)
+  got d (NoticeResponse m) = connLogMessage c m >> go d
+  got d m@(ReadyForQuery s) = do
+    s' <- atomicModifyIORef' sr ((,) s)
+    if s == s'
+      then go d
+      else done d m
+  got d m@(ErrorResponse _) = writeIORef sr StateUnsync >> done d m
+  got d m = done d m
+  done d m = Just m <$ next d
 
 -- |Receive the next message from PostgreSQL (low-level). Note that this will
 -- block until it gets a message.
@@ -534,10 +538,14 @@ pgSync c@PGConnection{ connState = sr } = do
   wait s = do
     r <- pgRecv s c
     case r of
-      Nothing -> do
-        pgSend c Sync
-        pgFlush c
-        wait True
+      Nothing
+        | s -> do
+          writeIORef sr StateClosed
+          fail $ "pgReceive: connection closed"
+        | otherwise -> do
+          pgSend c Sync
+          pgFlush c
+          wait True
       (Just (ErrorResponse{ messageFields = m })) -> do
         connLogMessage c m
         wait s

Database/PostgreSQL/Typed/Relation.hs
@@ -54,7 +54,7 @@ class (PGRep a, PGRecordType (PGRepType a)) => PGRelation a where
 -- If you want any derived instances, you'll need to create them yourself using StandaloneDeriving.
 --
 -- Requires language extensions: TemplateHaskell, FlexibleInstances, MultiParamTypeClasses, DataKinds, TypeFamilies, PatternGuards
-dataPGRelation :: String -- ^ Haskell type and constructor to create 
+dataPGRelation :: String -- ^ Haskell type and constructor to create
   -> PGName -- ^ PostgreSQL table/relation name
   -> (String -> String) -- ^ How to generate field names from column names, e.g. @("table_"++)@ (input is 'pgNameString')
   -> TH.DecsQ
@@ -144,7 +144,7 @@ dataPGRelation typs pgtab colf = do
             , TH.ConE 'Nothing)
           ])
         [] ]
-      , TH.FunD 'pgDecodeValue 
+      , TH.FunD 'pgDecodeValue
         [ TH.Clause [TH.WildP, TH.WildP, TH.ConP 'PGNullValue []]
           (TH.NormalB $ TH.ConE 'Nothing)
           []

postgresql-typed.cabal
@@ -3,7 +3,7 @@ Version:       0.5.0
 Cabal-Version: >= 1.8
 License:       BSD3
 License-File:  COPYING
-Copyright:     2010-2013 Chris Forno, 2014-2016 Dylan Simon
+Copyright:     2010-2013 Chris Forno, 2014-2017 Dylan Simon
 Author:        Dylan Simon
 Maintainer:    Dylan Simon <dylan-pgtyped@dylex.net>
 Stability:     provisional
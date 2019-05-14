b40bea3eb4c795bf89dac3cf9bb42b79cb1f7831
Alberto Valverde <alberto@toscat.net>
2019-05-14 17:51:57 +0200

    allow overriding socket path in tests

--- test/Connect.hs
+++ test/Connect.hs
@@ -1,14 +1,17 @@
 {-# LANGUAGE CPP, OverloadedStrings #-}
 module Connect where
 
+import Data.Maybe (fromMaybe)
 import Database.PostgreSQL.Typed (PGDatabase(..), defaultPGDatabase)
 import Network.Socket (SockAddr(SockAddrUnix))
+import System.Environment (lookupEnv)
+import System.IO.Unsafe (unsafePerformIO)
 
 db :: PGDatabase
 db = defaultPGDatabase
   { pgDBName = "templatepg"
 #ifndef mingw32_HOST_OS
-  , pgDBAddr = Right (SockAddrUnix "/tmp/.s.PGSQL.5432")
+  , pgDBAddr = Right (SockAddrUnix (fromMaybe "/tmp/.s.PGSQL.5432" (unsafePerformIO (lookupEnv "PGSOCK"))))
 #endif
   , pgDBUser = "templatepg"
   -- , pgDBDebug = True
